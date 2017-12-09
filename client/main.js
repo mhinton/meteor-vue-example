e9bcf8309a7bcab1cb844459872100c647d877e6
Matthew Hinton <mhinton@gmail.com>
2017-12-09 10:53:06 -0600

    Initial try - got styles but not components

--- client/main.js
+++ client/main.js
@@ -1,6 +1,10 @@
 // Libs
 import {Meteor} from 'meteor/meteor';
-import {Vue} from 'meteor/akryum:vue';
+import Vue from 'vue';
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+
+Vue.use(Vuetify);
 
 // Main app
 import App from '/imports/ui/App.vue';
--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -8,6 +8,24 @@
   <p>
     Learn more about the vue integration <a href="https://github.com/Akryum/meteor-vue-component">on GitHub</a>.
   </p>
+
+   <v-dialog v-model="dialog2" max-width="500px">
+      <v-card>
+        <v-card-title>
+          Dialog 2
+        </v-card-title>
+        <v-card-text>
+          <v-select
+            v-bind:items="select"
+            label="A Select List"
+            item-value="text"
+          ></v-select>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="primary" flat @click.stop="dialog2 = false">Close</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
 </div>
 </template>
 
@@ -26,7 +44,9 @@ export default {
   data() {
     return {
       buttonLabel: 'Click me!',
-      count: 0
+      count: 0,
+      dialog2: false,
+      select: ["Item 1", "Item 2", "Item 3"]
     }
   },
   meteor: {
